81b30c7a8898937e8adcb62bd2f6469bfc5c6c33
Wallk790walclear <wallk790@newschool.edu>
2015-05-11 00:41:40 -0400

    Using Node.js to solve library conflict

---
+++ app.js
@@ -0,0 +1,59 @@
+// Install via: 
+// 		npm install sentiment
+// 		npm install request
+// then to run, type:
+// 		node app.js
+
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 1337;
+var express = require("express");
+var sentiment = require('sentiment');
+var request = require('request');
+
+// Express web server
+var app = express();
+//app.use(app.router); //use both root and other routes below
+app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Root!!");
+});
+
+
+app.get("/getData", function(req, response){
+	console.log(req.query.url);
+	var url = req.query.url;
+	// Reuqests the NYTimes data
+	request('http://api.nytimes.com/svc/community/v3/user-content/url.json?api-key=5a201de4998d3b4049fd577cce90db66:1:71926099&url='+url, function (error, res, data) {
+		if (!error && res.statusCode == 200) {
+			data = JSON.parse(data);
+
+			// iterate over each comment
+			for (var i = 0; i < data.results.comments.length; i++) {
+
+				// get the comment body
+				var comment = data.results.comments[i];
+				var body = comment.commentBody;
+
+				// check the sentiment of the comment
+				var s = sentiment(body);
+				comment["sentiment"] = s;
+			}
+			response.jsonp(data);
+		}
+		else {
+			response.send("Something broke! :(");
+		}
+		
+	});
+
+});
+
+app.listen(port, host);
+
+
+
+
+
+
